177a5916e343253494dd239169bff4baa9ab50f9
Ge Luxue <lge5@slb.com>
2020-11-25 15:19:35 +0800

    refactor: use mock to get IUserStore and IArticleStore

--- MiniBlog/Stores/UserStoreWillReplaceInFuture.cs
+++ MiniBlog/Stores/UserStoreWillReplaceInFuture.cs
@@ -1,14 +1,42 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Components.Server;
 using MiniBlog.Model;
 
 namespace MiniBlog.Stores
 {
+    public interface IUserStore
+    {
+        List<User> Users { get; }
+    }
+
+    public class UserStore : IUserStore
+    {
+        public List<User> Users
+        {
+            get
+            {
+                return UserStoreWillReplaceInFuture.Users;
+            }
+        }
+    }
+
+    /*
+    public class TestUserStore : IUserStore
+    {
+        public List<User> Users => throw new Exception();
+    }
+    */
+
     public class UserStoreWillReplaceInFuture
     {
-        public UserStoreWillReplaceInFuture()
+        static UserStoreWillReplaceInFuture()
         {
             Users = new List<User>();
+        }
+
+        public UserStoreWillReplaceInFuture()
+        {
         }
 
         public static List<User> Users { get; private set; }
--- MiniBlogTest/ControllerTest/UserControllerTest.cs
+++ MiniBlogTest/ControllerTest/UserControllerTest.cs
@@ -8,9 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using MiniBlog;
 using MiniBlog.Model;
 using MiniBlog.Stores;
+using Moq;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -63,7 +65,19 @@ namespace MiniBlogTest.ControllerTest
         [Fact]
         public async Task Should_register_user_fail_when_UserStore_unavailable()
         {
-            var client = GetClient();
+            Mock<IUserStore> mockArticleStore = new Mock<IUserStore>();
+            mockArticleStore.Setup(mock => mock.Users).Throws<Exception>();
+            var client = Factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.AddSingleton<IUserStore>((serviceProvider) =>
+                    {
+                        return mockArticleStore.Object;
+                    });
+                });
+            }).CreateClient();
+            //var client = GetClient();
 
             var userName = "Tom";
             var email = "a@b.com";
